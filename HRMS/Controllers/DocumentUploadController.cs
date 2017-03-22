1a15e0f831dba22a55f7a2aa9221b4a73f0c2152
JPithadiya <jpithadiya@affirma.com>
2017-03-22 15:24:50 +0530

    Commit

--- HRMS/Controllers/DocumentUploadController.cs
+++ HRMS/Controllers/DocumentUploadController.cs
@@ -23,45 +23,7 @@ namespace HRMS.Controllers
             return View(obj);
         }
 
-        public JsonResult AjaxUpload(HttpPostedFileBase file, int EmployeeID, int DocumentTypeID)
-        {
-            Document entity = new Document();
-            entity.DocumentContent = new byte[file.InputStream.Length];
-            file.InputStream.Read(entity.DocumentContent, 0, entity.DocumentContent.Length);
-            entity.DocumentName = file.FileName;
-
-            DocumentService service = new DocumentService();
-            service.Create(entity);
-            int id = service.SaveChangesReturnId(entity);
-
-            EmployeeDocument ed = new EmployeeDocument();
-            ed.EmployeeID = EmployeeID;
-            ed.DocumentTypeID = DocumentTypeID;
-            ed.DocumentName = file.FileName;
-
-
-            return Json("Success", JsonRequestBehavior.AllowGet);
-
-
-        }
-
-        public FileContentResult FileDownload(int id)
-        {
-            //declare byte array to get file content from database and string to store file name
-            byte[] fileData;
-            string fileName;
-
-            var record = from p in dbContext.Documents
-                         where p.DocumentID == id
-                         select p;
-            //only one record will be returned from database as expression uses condtion on primary field
-            //so get first record from returned values and retrive file content (binary) and filename
-            fileData = (byte[])record.First().DocumentContent.ToArray();
-            fileName = record.First().DocumentName;
-            //return file and provide byte file content and file name
-            return File(fileData, "text", fileName);
-
-        }
+       
 
     }
 }
--- HRMS/Controllers/EmployeeDocumentController.cs
+++ HRMS/Controllers/EmployeeDocumentController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using ViewModel;
+using Service;
 namespace HRMS
 {
     public class EmployeeDocumentController : Controller
@@ -11,8 +12,54 @@ namespace HRMS
         // GET: EmployeeDocument
         public ActionResult Index()
         {
-            ViewBag.EmployeeID =HttpContext.Request.QueryString["EmployeeID"];
-            return View();
+            
+            MasterViewModel obj = new MasterViewModel();
+            Service.DocumentTypeService service = new Service.DocumentTypeService();
+            obj.DocumentTypes = service.Get().ToList();
+
+
+            return View(obj);
+            
+        }
+
+        public JsonResult AjaxUpload(HttpPostedFileBase file, int EmployeeID, int DocumentTypeID)
+        {
+            Document entity = new Document();
+            entity.DocumentContent = new byte[file.InputStream.Length];
+            file.InputStream.Read(entity.DocumentContent, 0, entity.DocumentContent.Length);
+            entity.DocumentName = file.FileName;
+
+            DocumentService service = new DocumentService();
+            service.Create(entity);
+            int id = service.SaveChangesReturnId(entity);
+
+            EmployeeDocument ed = new EmployeeDocument();
+            ed.EmployeeID = EmployeeID;
+            ed.DocumentTypeID = DocumentTypeID;
+            ed.DocumentName = file.FileName;
+
+
+            return Json("Success", JsonRequestBehavior.AllowGet);
+
+
+        }
+
+        public FileContentResult FileDownload(int id)
+        {
+            //declare byte array to get file content from database and string to store file name
+            byte[] fileData;
+            string fileName;
+
+            var record = from p in dbContext.Documents
+                         where p.DocumentID == id
+                         select p;
+            //only one record will be returned from database as expression uses condtion on primary field
+            //so get first record from returned values and retrive file content (binary) and filename
+            fileData = (byte[])record.First().DocumentContent.ToArray();
+            fileName = record.First().DocumentName;
+            //return file and provide byte file content and file name
+            return File(fileData, "text", fileName);
+
         }
     }
 }
