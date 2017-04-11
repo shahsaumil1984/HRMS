b6c81e4cb2564244c659888030a929b854ba8d5f
JPithadiya <jpithadiya@affirma.com>
2017-04-11 10:56:28 +0530

    Commit

---
+++ HRMS/Scripts/Services/EmployeeStatusService.js
@@ -0,0 +1,18 @@
+﻿
+function EmployeeStatusService() { } EmployeeStatusService.prototype = new Proxy('EmployeeStatus', 'StatusID');
+
+// Example Custom Method
+EmployeeStatusService.prototype.MyCustomMethod = function (params, callback) {
+
+    // Query allows for custom get calls to the server    
+
+    // Query API
+    this.Query('MyCustomMethodNameOnThisObject?' + params, callback);
+
+    // Query Controller
+    this.QueryController('MyCustomMethodNameOnThisObject?' + params, callback);
+
+}
+
+var EmployeeStatusService = new EmployeeStatusService();
+
