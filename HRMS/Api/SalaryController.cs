8b0904d04f171dbc85dacf9ac14fe8f8b0124cc8
shahsaumil1984 <saumil.shah@alept.com>
2017-04-19 10:44:53 +0530

    Commit

--- HRMS/Api/SalaryController.cs
+++ HRMS/Api/SalaryController.cs
@@ -470,7 +470,7 @@ namespace Api
                         body = body + "PFA for the salary slip for the month of " + salaryMonth + " " + salaryYear + ".</br></br>";
                         body = body + "Thanks & Regards,</br>Richa Nair</br>Practice Lead – HR</br>Alept Consulting Private LimitedPh: +91 7574853588 | URL: www.alept.com</br>B - 307/8/9, Mondeal Square, S.G.Highway Road, Prahladnagar, Ahmedabad, Gujarat - 380015";
 
-                        string attachment = Convert.ToBase64String(ExportToPdf(item, item.Employee.FullName, salaryMonth));
+                        string attachment = Convert.ToBase64String(ExportToPdf(item));
                         bool isMailSent = Helper.SendEmailToEmployee(Subject, body, fromEmailAddress, fromEmailUser, toEmailAdd, toEmailUser, attachment);
                     }
                 }
