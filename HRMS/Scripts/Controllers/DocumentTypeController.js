0016a192213450f84aead640d4e80488085291fe
NamrataNegi <namrata.negi@alept.com>
2017-04-24 18:35:15 +0530

    UI bug fixes

--- HRMS/Scripts/Controllers/DocumentTypeController.js
+++ HRMS/Scripts/Controllers/DocumentTypeController.js
@@ -23,10 +23,10 @@ _.documentTypeDeleteCallback = function (status, data)
 {
     if (status)
     {
-        alert('Successfully deleted document type');
+        toastr.success('Successfully deleted document type');
     }
     else{
         
-        alert('Cannot delete the document type as it is already in use');
+        toastr.error('Cannot delete the document type as it is already in use');
     }
 }
