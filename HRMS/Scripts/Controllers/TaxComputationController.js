f3dbbf6a507d3365501bd1c74c518cce5b785963
NamrataNegi <namrata.negi@alept.com>
2017-04-28 11:40:17 +0530

    UI changes

--- HRMS/Scripts/Controllers/TaxComputationController.js
+++ HRMS/Scripts/Controllers/TaxComputationController.js
@@ -9,28 +9,41 @@
 // Event Handlers
 //----------------------------------------------
 $(document).ready(function () {
-    _.Initialize(function (status, msg) {      
+    _.Initialize(function (status, msg) {
     });
 });
 
 // Form Validation
 //----------------------------------------------
 _.NewTaxComputationForm = function (formName, hideList, callback) {
-    
+
     var fb = this.FormBindings[formName];
-    if (fb.detailsWindow) {
-        fb.validator.resetForm();
-        fb.ShowForm(hideList);
-    }
-    var df = fb.detailsWindow.find("[data-default-focus='true']");
-    if (df != null && df.length > 0) {
-        setTimeout('$("#' + df[0].id + '").focus()', 400);
-    }
-    debugger;
-    if (taxComputationDetailsForm.model().Year == 0) {
-        taxComputationDetailsForm.SetValue('Year', null);
-    }
-    taxComputationDetailsForm.SetValue('EmployeeID', $('#hdnEmployeeID').val());
+
+    fb.New(function (status, data) {
+
+        if (fb.detailsWindow) {
+            fb.validator.resetForm();
+            fb.ShowForm(hideList);
+        }
+        var df = fb.detailsWindow.find("[data-default-focus='true']");
+        if (df != null && df.length > 0) {
+            setTimeout('$("#' + df[0].id + '").focus()', 400);
+        }
+        
+        if (taxComputationDetailsForm.model().Year == 0) {
+            taxComputationDetailsForm.SetValue('Year', null);
+        }
+        taxComputationDetailsForm.SetValue('EmployeeID', $('#hdnEmployeeID').val());
+    });
+    //if (fb.detailsWindow) {
+    //    fb.validator.resetForm();
+    //    fb.ShowForm(hideList);
+    //}
+    //var df = fb.detailsWindow.find("[data-default-focus='true']");
+    //if (df != null && df.length > 0) {
+    //    setTimeout('$("#' + df[0].id + '").focus()', 400);
+    //}
+
 }
 
 _.ResetForm = function () {
