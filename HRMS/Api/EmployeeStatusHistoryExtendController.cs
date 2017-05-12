decdf41c3c08640684a1f2fae7885fead5e0b868
NamrataNegi <namrata.negi@alept.com>
2017-05-12 17:38:56 +0530

    Applied security on API call

--- HRMS/Api/EmployeeStatusHistoryExtendController.cs
+++ HRMS/Api/EmployeeStatusHistoryExtendController.cs
@@ -5,8 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
 using System.Net.Http;
+using System.Web.Http;
 
 namespace HRMS.Api
 {
